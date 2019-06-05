389ad5898f673e92e411581e07f42ff0f124df20
Ricardo Romo <jricardoromo21@gmail.com>
2019-06-05 00:28:43 -0500

    models

---
+++ neural_class.py
@@ -0,0 +1,30 @@
+
+
+import face_recognition
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+
+
+class NeuralClass(frame):
+    def __init__(self, frame):
+        self.frame = frame
+        self.location = self.detect()
+
+    def detect(self):
+        pass
+    def encode(self):
+        pass
+    def compare(self):
+        pass
+    def race(self):
+        pass
+    def glass(self):
+        pass
+    def age(self):
+        pass
+    def hair(self):
+        pass
+    def gender(self):
+        pass
