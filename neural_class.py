181ffa388ad0807026f912b8594a4c791c25e38c
Ricardo Romo <jricardoromo21@gmail.com>
2019-06-05 00:37:15 -0500

    format

--- neural_class.py
+++ neural_class.py
@@ -7,24 +7,32 @@ import matplotlib.pyplot as plt
 from collections import Counter
 
 
-class NeuralClass(frame):
+class NeuralClass:
+
     def __init__(self, frame):
         self.frame = frame
         self.location = self.detect()
 
     def detect(self):
         pass
+
     def encode(self):
         pass
+
     def compare(self):
         pass
+
     def race(self):
         pass
+
     def glass(self):
         pass
+
     def age(self):
         pass
+
     def hair(self):
         pass
+
     def gender(self):
         pass
